947b6ec9deb48f0427ae0d5c07880e14ebb8f07c
Alga <58613731+algavania@users.noreply.github.com>
2020-09-21 00:56:46 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -12,18 +12,18 @@ function trialClick() {
     if (fname.value === '') {
         isError = true;
         fname.style.border = "2px solid red";
-        errorMsg['fname'].style.display = 'block';
+        errorMsg[0].style.display = 'block';
         imgError[0].style.display = 'block';
     } else {
         isError = false;
         fname.style.border = "1px solid lightgray";
-        errorMsg['fname'].style.display = 'none';
+        errorMsg[0].style.display = 'none';
     }
 
     if (lname.value === '') {
         isError = true;
         lname.style.border = "2px solid red";
-        errorMsg['lname'].style.display = 'block';
+        errorMsg[1].style.display = 'block';
         if (fname.value != '') {
             imgError[1].style.marginTop = '0';
         }
@@ -31,16 +31,16 @@ function trialClick() {
     } else {
         isError = false;
         fname.style.border = "1px solid lightgray";
-        errorMsg['lname'].style.display = 'none';
+        errorMsg[1].style.display = 'none';
     }
 
     if (email.value === '' || !validateEmail(email.value)) {
         isError = true;
         if (!validateEmail(email.value)) {
-            errorMsg['email'].innerHTML = 'Looks like this is not an email';
-            errorMsg['email'].style.display = 'block';
+            errorMsg[2].innerHTML = 'Looks like this is not an email';
+            errorMsg[2].style.display = 'block';
         } else {
-            errorMsg['email'].style.display = 'none';
+            errorMsg[2].style.display = 'none';
         }
         if (lname.value != '') {
             imgError[2].style.marginTop = '0';
@@ -50,24 +50,24 @@ function trialClick() {
     } else {
         isError = false;
         fname.style.border = "1px solid lightgray";
-        errorMsg['email'].style.display = 'none';
+        errorMsg[2].style.display = 'none';
     }
 
     if (password.value === '' || password.value.length < 6) {
         isError = true;
         if (password.value != '' && password.value.length < 6) {
-            errorMsg['password'].innerHTML = 'Password must be at least 6 characters'
+            errorMsg[3].innerHTML = 'Password must be at least 6 characters'
         }
         if (validateEmail(email.value)) {
             imgError[3].style.marginTop = '0';
         }
-        errorMsg['password'].style.display = 'block';
+        errorMsg[3].style.display = 'block';
         password.style.border = "2px solid red";
         imgError[3].style.display = 'block';
     } else {
         isError = false;
         fname.style.border = "1px solid lightgray";
-        errorMsg['password'].style.display = 'none';
+        errorMsg[3].style.display = 'none';
     }
 
     if (fname.value != '' && lname.value != '' && validateEmail(email.value) && password.value.length >= 6) {
